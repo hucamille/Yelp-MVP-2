07941cf7621fc9a16acca81aa19013910eda52fa
Camille Hug <hug.camille@hotmail.fr>
2022-02-08 20:36:00 +0100

    Controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,11 +16,9 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
     respond_to do |format|
       if @restaurant.save
-        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
-        format.json { render :show, status: :created, location: @restaurant }
+        redirect_to @restaurant, notice: 'Restaurant was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
